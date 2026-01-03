26f24f6597de6d5d8e1bb03fdd773f18802b47e1
Syed Shariq <25cs1940@amceducation.in>
2026-01-03 23:50:01 +0530

    Add responsivness in the calculator UI

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -32,20 +32,20 @@ const Calculator = () => {
     }
 
   return (
-    <div className='h-full w-full gap-10 flex flex-col  items-center justify-center'>
-        <h1 className='text-5xl bg-linear-150 from-gray-200 to-gray-700 text-transparent bg-clip-text'>Calculator</h1>
-        <div className='h-[70vh] w-[20vw] p-7 border-2 border-white/30 shadow-[inset_0_0_8px] bg-[#2d3238] rounded-3xl flex flex-col '>
-            <div className='h-[20vh] flex items-end flex-col justify-center bg-[#282a2d] border-white/30 p-3 border-2 rounded-2xl'>
-                <div className='text-2xl text-gray-200 opacity-70'>{expression}</div>
-                <div className='text-4xl text-gray-200 font-bold'>{result.length > 15 ? result.slice(0, 15) : result}</div>
+    <div className='h-screen w-screen gap-3 sm:gap-6 lg:gap-10 flex flex-col items-center justify-center p-3 sm:p-4 lg:p-0 overflow-hidden'>
+        <h1 className='text-2xl sm:text-3xl lg:text-5xl bg-linear-150 from-gray-200 via-gray-700 to-gray-200 text-transparent bg-clip-text flex-shrink-0'>Calculator</h1>
+        <div className='h-auto w-full sm:w-[90vw] lg:h-[70vh] lg:w-[20vw] max-w-[420px] lg:max-w-none p-3 sm:p-5 lg:p-7 border-2 border-white/30 shadow-[inset_0_0_8px] bg-[#2d3238] rounded-3xl flex flex-col flex-shrink-0'>
+            <div className='min-h-[60px] sm:min-h-[80px] lg:min-h-[120px] flex items-end flex-col justify-center bg-[#282a2d] border-white/30 p-2 sm:p-3 border-2 rounded-2xl mb-3 sm:mb-4 flex-shrink-0'>
+                <div className='text-xs sm:text-base lg:text-2xl text-gray-200 opacity-70 text-right break-words w-full max-h-[20px] sm:max-h-[28px] lg:max-h-[35px] overflow-hidden'>{expression}</div>
+                <div className='text-lg sm:text-2xl lg:text-4xl text-gray-200 font-bold text-right max-h-[28px] sm:max-h-[40px] lg:max-h-[50px] overflow-hidden'>{result.length > 15 ? result.slice(0, 15) : result}</div>
             </div>
-            <div className='h-full mt-4 text-5xl grid grid-cols-4 '>
+            <div className='grid grid-cols-4 gap-1 sm:gap-2 lg:gap-4 flex-1'>
                {buttons.map((btn) => (
                 <button key={btn} 
                 onClick={() => {
                     handleClick(btn)
                 }}
-                className={`${btn === "=" ? "bg-linear-150 from-gray-200 via-gray-600 to-gray-200 hover:brightness-110 transition-all duration-300 " : "bg-[#282a2d]"} cursor-pointer transition duration-300 hover:bg-[#323437] shadow-black shadow-2xl pb-2 h-[8vh] w-[4vw]  rounded-full`}
+                className={`${btn === "=" ? "bg-linear-150 from-gray-200 via-gray-600 to-gray-200 hover:brightness-110" : "bg-[#282a2d] hover:bg-[#323437]"} cursor-pointer transition duration-300 shadow-black shadow-2xl aspect-square rounded-full flex items-center justify-center font-semibold text-xl sm:text-sm lg:text-4xl text-gray-200 min-h-10 sm:min-h-12.5 lg:min-h-auto`}
                 >{btn}</button>
                ))}
             </div>
